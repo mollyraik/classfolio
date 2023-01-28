8cae54d111dc4720685f28e3b997d9ea8905c8e6
Molly Raik <molly.raik@gmail.com>
2023-01-28 17:21:41 -0500

    wip: adds project schema

---
+++ models/project.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    photo: {
+        type: String,
+        default: '/imgs/default-project'
+    },
+    dateMade: {
+        type: Date,
+        // default: new Date().now(),
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'Student'
+    },
+}, {timestamps: true});
+
+module.exports = mongoose.model('Project', projectSchema);
