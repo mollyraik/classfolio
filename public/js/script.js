68d6e82af1817a28eacf75ac78286229842fa19d
Molly Raik <molly.raik@gmail.com>
2023-02-07 17:49:51 -0500

    wip: tests some css stying and masonry fixes

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,42 @@
-window.onload = (event) => {
-        // Preloading images
-        let images = document.querySelectorAll("img");
-        let imagesCount = images.length;
-        let imagesLoaded = 0;
-        images.forEach(img => {
-          img.addEventListener("load", () => {
-            imagesLoaded++;
-            if (imagesLoaded === imagesCount) {
-              // All images have finished loading, apply stylesheet
-              console.log('page fully loaded');
-            }
-          });
-        }); 
-    
-}
+// $(document).ready(function() {
+//     var images = $('.row img');
+//     var loaded = 0;
+  
+//     images.each(function() {
+//       var image = new Image();
+//       image.src = $(this).attr('src');
+//       image.onload = function() {
+//         loaded++;
+//         if (loaded == images.length) {
+//           // All images have finished loading, so initialize Masonry
+//           $('.row').masonry({
+//             itemSelector: '.col-xs-13',
+//             columnWidth: '.col-xs-12',
+//             gutter: '.g-3'
+//           });
+//         }
+//       };
+//     });
+//   });
+  
+
+
+// window.onload = (event) => {
+//         // Preloading images
+//         let images = document.querySelectorAll("img");
+//         let imagesCount = images.length;
+//         let imagesLoaded = 0;
+//         images.forEach(img => {
+//           img.addEventListener("load", () => {
+//             imagesLoaded++;
+//             if (imagesLoaded === imagesCount) {
+//               // All images have finished loading, apply stylesheet
+//               console.log('page fully loaded');
+//             }
+//           });
+//         }); 
+        
+// }
 
 
 
